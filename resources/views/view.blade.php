0faef42160121b347e85290cc57325b85519ff1e
Chris Toxz <1christix@gmail.com>
2022-01-02 15:43:47 +0100

    style: change view pagge

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -5,6 +5,9 @@
         <div class="container">
 
             <div class="card text-center">
+                <div class="card-header">
+                    {{ $video->title }}
+                </div>
                 <div class="card-body">
                     <p><video controls autoplay muted loop >
                             <source src="{{ asset('/storage/videos/'.$video->file) }}" type="video/mp4">
@@ -25,10 +28,10 @@
                     </div>
                     <div class="row">
                         <div class="col-md-3">
-                            {{ $video->created_at }}
+                            Uploaded at: {{ $video->created_at }}
                         </div>
                         <div class="col-md-6">
-                            {{ $video->title }}
+
                         </div>
                         <div class="col-md-3">
                             {{  $video->views()->count() }} views
@@ -36,6 +39,32 @@
                     </div>
                 </div>
             </div>
+            <div class="row">
+                <div class="card" style="width: 25rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Date info</h5>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Uploaded at: {{ $video->created_at }}</li>
+                        <li class="list-group-item">Optimization finished at:</li>
+                        <li class="list-group-item"></li>
+                    </ul>
+                </div>
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Video details</h5>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Duration: {{ $video->duration_string }}</li>
+                        <li class="list-group-item">Original size: {{ $video->size }} MB</li>
+                        <li class="list-group-item">Resolution: {{ $video->resolution_string }}</li>
+                        <li class="list-group-item">@if ($video->job->is_finished) Optimized! @else Optimization in process {{ $video->job->progress_now }} @endif</li>
+                        <li class="list-group-item">A second item</li>
+                        <li class="list-group-item">A third item</li>
+                    </ul>
+                </div>
+            </div>
         </div>
+
     </div>
 @endsection
