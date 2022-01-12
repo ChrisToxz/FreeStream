dd0a9c1e48e88188cbbae07b272b15653e1ec9e8
Chris Toxz <1christix@gmail.com>
2022-01-12 21:19:21 +0100

    feat: Video edit modal

--- app/Http/Livewire/Video.php
+++ app/Http/Livewire/Video.php
@@ -2,153 +2,44 @@
 
 namespace App\Http\Livewire;
 
-use App\Events\videoUploaded;
-use App\Jobs\ConvertVideo;
-use App\Jobs\ConvertVideoForStreaming;
+use App\Http\Controllers\VideoController;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\View;
-use Illuminate\Support\Str;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 use Livewire\Component;
-use Livewire\WithFileUploads;
-use ProtoneMedia\LaravelFFMpeg\Support\FFMpeg;
 
 class Video extends Component
 {
-    use WithFileUploads;
+    public $perPage = 8000;
+    protected $listeners = [
+        'load-more' => 'loadMore',
+        'videosRefresh' => '$refresh',
+    ];
 
     public $video, $title, $type;
 
     public function render()
     {
-        return view('livewire.video');
+        $videos = \App\Models\Video::latest()->get();
+        return view('livewire.load-videos', ['videos' => $videos]);
     }
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function edit(\App\Models\Video $video)
     {
-        //
+
+        $this->title = $video->title;
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function update()
     {
-        //
+        dd($this->title);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function destroy(\App\Models\Video $video)
     {
-        $this->validate([
-            'video' => 'required|file|mimetypes:video/mp4,video/mpeg,video/x-matroska',
-            'type' => 'required'
-        ]);
-
-        $tag = Str::random(4); // generate tag
-        $title = $this->title ?? $this->video->getClientOriginalName();
-        $hash = $this->video->hashName(); // generator hash
-
-        // Create Thumb
-        $media = FFMpeg::openUrl($this->video->path());
-        $media->getFrameFromSeconds(0.1)->export()->toDisk('videos')->save($tag.'/thumb.jpg');
-
-        $this->video->storeAs($tag, $hash, 'videos'); // store original video
-
-        $video = \App\Models\Video::create(array(
-            'tag' => $tag,
-            'hash' => $hash,
-            'title' => $title,
-            'type'  => $this->type,
-        ));
-
-        videoUploaded::dispatch($video);
-
-        $this->dispatchBrowserEvent('resetform');
-        $this->emit('videosRefresh');
-
-        switch ($this->type){
-            case 0://original
-                $video->files = ['mp4' => $hash];
-                $video->save();
-                toastr()->livewire()->title('Success!')->addSuccess('Video uploaded!');
-                break;
-            case 1: //x264
-                ConvertVideo::dispatch($video);
-                toastr()->livewire()->title('Success!')->addSuccess('Video uploaded, being processed now!');
-                break;
-            case 2://streamable
-                ConvertVideoForStreaming::dispatch($video);
-                toastr()->livewire()->title('Success!')->addSuccess('Video uploaded, being processed now!');
-                break;
-
-        }
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Video  $video
-     * @return \Illuminate\Http\Response
-     */
-    public function show(\App\Models\Video $video)
-    {
-
-        switch ($video->type){
-            case 0:
-                return View::make('view')->with(['video' => $video]);
-                break;
-            case 1:
-                return View::make('view')->with(['video' => $video]);
-                break;
-            case 2:
-                return View::make('view2')->with(['video' => $video]);
-                break;
-        }
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Video  $video
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Video $video)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Video  $video
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Video $video)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Video  $video
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Video $video)
-    {
-        //
+        Storage::disk('videos')->deleteDirectory($video->tag);
+        $video->delete();
+        toastr()->livewire()->title('Success!')->addSuccess('Video deleted!');
+//        $this->emit('videosRefresh');
     }
 }
--- resources/views/livewire/load-videos.blade.php
+++ resources/views/livewire/load-videos.blade.php
@@ -1,7 +1,61 @@
-<div wire:poll.1ms class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3" id="results">
+<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3" id="results">
     @foreach($videos as $video)
-        <div class="col" id="rBEX"
-             x-data="{ isReady: {{ $video->finished }} }">
+        <div class="col" id="rBEX" x-data="{ isReady: {{ $video->finished }}, modal{{ $video->tag }}: false  }">
+            <div x-show="modal{{ $video->tag }}" class="modal-backdrop show"></div>
+            <div x-show="modal{{ $video->tag }}">
+                <div class="modal fade show" style="display: block;" >
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="edit-{{ $video->tag }}">Edit video: {{ $video->title }} </h5><small class="float-end">Tag: {{ $video->tag }}</small>
+                            </div>
+                            <div class="modal-body">
+                                <form>
+                                    <div class="form-group">
+                                        <label for="title">Title</label>
+                                        <input type="text" class="form-control" id="title" wire:model="title">
+                                        @error('title') <span class="text-danger error">{{ $message }}</span>@enderror
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="tag">Tag</label>
+                                        <input type="text" disabled class="form-control" id="tag" value="{{ $video->tag }}" >
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="tag">Hash</label>
+                                        <input type="text" disabled class="form-control" id="hash" value="{{ $video->hash }}" >
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="tag">Stream hash</label>
+                                        <input type="text" disabled class="form-control" id="streamhash" value="{{ $video->streamhash }}" >
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="tag">Files</label>
+                                        <textarea disabled class="form-control" id="files">@foreach($video->files as $file) {{ $file }} &#013; &#010; @endforeach</textarea>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="tag">Custom link</label>
+                                        <input type="text" disabled class="form-control" id="slug" value="Slug" >
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="tag">Retention policy</label>
+                                        <select class="form-control">
+                                            <option>X views</option>
+                                            <option>Date</option>
+                                        </select>
+                                    </div>
+
+
+                                </form>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" @click="modal{{ $video->tag }} = false" data-bs-dismiss="modal">Close</button>
+                                <button type="button" wire:click.prevent="update()" data-dismiss="modal" class="btn btn-primary">Save changes</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             <div class="card shadow-sm h-100">
                 <a href="http://localhost/v/{{ $video->tag }}">
                     <img class="card-img-top" style="    width: 100%; height: 15vw; object-fit: cover;" src="{{ asset('/storage/videos/'.$video->tag.'/thumb.jpg') }}" alt="Thumbnail"></a>
@@ -17,14 +71,17 @@
                         </p>
                         @else
                             <p class="card-text">
-                                <button class="btn btn-sm btn-outline-secondary" id="button" value="edit">Edit</button>
-                                <button type="button" class="btn btn-sm btn-outline-danger" id="button" value="delete">Delete</button>
+                                <button @click="modal{{ $video->tag }} = true" wire:click="edit('{{ $video->tag }}')" class="btn btn-sm btn-outline-secondary" >Edit</button>
+                                <button wire:click="destroy('{{ $video->tag }}')" type="button" class="btn btn-sm btn-outline-danger" id="button" value="delete">Delete</button>
                                 <small class="text-muted float-end" >0 MB - 0 views</small>
                             </p>
                         @endif
 
                 </div>
             </div>
+
         </div>
+
     @endforeach
+
 </div>
