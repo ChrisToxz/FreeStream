3213601688ec20f0b662b33860cf84b395848c5f
Chris Toxz <1christix@gmail.com>
2022-01-22 23:23:56 +0100

    video model + migration

---
+++ database/migrations/2022_01_22_221606_create_videos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVideosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('videos', function (Blueprint $table) {
+//            $table->id();
+            $table->string('tag')->primary();
+            $table->string('title');
+            $table->string('original'); // original video hash filename
+            $table->string('streamhash'); // filename/path of mp4/m3u8
+            $table->enum('type', \App\Enums\VideoType::getValues())->default(\App\Enums\VideoType::Original());
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('videos');
+    }
+}
