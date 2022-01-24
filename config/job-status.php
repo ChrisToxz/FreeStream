8980809c9bde210d5e00d3aac4889cb0b8c78522
Chris Toxz <1christix@gmail.com>
2022-01-24 19:57:47 +0100

    clean master

--- config/job-status.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'model' => \Imtigger\LaravelJobStatus\JobStatus::class,
-    'event_manager' => \Imtigger\LaravelJobStatus\EventManagers\DefaultEventManager::class,
-    'database_connection' => null
-];
