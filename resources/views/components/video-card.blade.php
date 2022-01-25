32151239da72ee36b9df983e2a6c20677ce8d708
Chris Toxz <1christix@gmail.com>
2022-01-25 20:20:22 +0100

    button & type icon update video-card

--- resources/views/components/video-card.blade.php
+++ resources/views/components/video-card.blade.php
@@ -4,10 +4,17 @@
             <img class="card-img-top" style="    width: 100%; height: 15vw; object-fit: cover;" src="{{ asset('/storage/videos/'.$video->tag.'/thumb.jpg') }}" alt="Thumbnail"></a>
         <div class="card-body">
             <h5 class="card-title">
-                {{ $video->title }} <small data-bs-toggle="tooltip" data-bs-placement="top" title="x264 + HLS"><i class="bi-cast"></i></small>
+                {{ $video->title }}
+                @if($video->type == 1)
+                    <i data-bs-toggle="tooltip" data-bs-placement="top" title="Original file" class="bi-file-play"></i>
+                @elseif($video->type == 2)
+                    <i data-bs-toggle="tooltip" data-bs-placement="top" title="x264 (Web)" class="bi-cloud"></i>
+                @elseif($video->type == 3)
+                    <small data-bs-toggle="tooltip" data-bs-placement="top" title="x264 + HLS"><i class="bi-cast"></i></small>
+                @endif
                 <small class="text-muted float-end">00:00</small>
             </h5>
-            <h6 class="card-subtitle mb-2 text-muted">{{ \Carbon\Carbon::parse($video->created_at)->diffForHumans() }}</h6>
+            <h6 class="card-subtitle mb-2 text-muted">{{ \Carbon\Carbon::parse($video->created_at)->diffForHumans() }} <small class="text-muted float-end">0 MB - 0 views</small></h6>
 
 {{--            <p class="text-center">--}}
 {{--            <div class="progress">--}}
@@ -15,10 +22,10 @@
 {{--            </div>--}}
 {{--            </p>--}}
             <p class="card-text">
-                <button class="btn btn-sm btn-outline-primary" data-bs-toggle="tooltip" data-bs-placement="top" title="Tag: {{$video->tag}}">Copy link</button>
-                <button class="btn btn-sm btn-outline-secondary" wire:click="$emit('showModal', 'edit-video', '{{ $video->tag }}')">Edit</button>
-                <button type="button" class="btn btn-sm btn-outline-danger" id="button" value="delete">Delete</button>
-                <small class="text-muted float-end">0 MB - 0 views</small>
+                <button class="btn btn-sm btn-outline-info bi-clipboard" data-bs-toggle="tooltip" data-bs-placement="top" title="Tag: {{$video->tag}}"> Copy link</button>
+                <button class="btn btn-sm btn-outline-primary bi-cloud-download" data-bs-toggle="tooltip" data-bs-placement="top" title="Tag: {{$video->tag}}"> Download</button>
+                <button class="btn btn-sm btn-outline-secondary bi-pencil-fill" wire:click="$emit('showModal', 'edit-video', '{{ $video->tag }}')"> Edit</button>
+                <button type="button" class="btn btn-sm btn-outline-danger bi-trash-fill" id="button" value="delete"> Delete</button>
             </p>
         </div>
     </div>
