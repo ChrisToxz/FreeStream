ee293e6784f486a8e502c35ab13c54a41acef1e1
Chris Toxz <1christix@gmail.com>
2022-01-27 20:05:44 +0100

    register checkRetention console command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,11 +2,17 @@
 
 namespace App\Console;
 
+use App\Console\Commands\checkRetention;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+
+    protected $commands = [
+        checkRetention::class
+    ];
+
     /**
      * Define the application's command schedule.
      *
