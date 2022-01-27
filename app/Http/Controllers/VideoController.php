13a6944c36717f12630c41a0811d6ff44c874697
Chris Toxz <1christix@gmail.com>
2022-01-27 19:02:36 +0100

    retention

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\RetentionType;
 use App\Models\Video;
 use Illuminate\Http\Request;
 
@@ -13,6 +14,15 @@ class VideoController extends Controller
     {
         //TODO: Move to middleware
         $video->addView();
+
+        //TODO: Rewrite
+        if($video->retention){
+            if($video->retention->type == RetentionType::Views()){
+                if($video->retention->value <= $video->views()->count()){
+                    dd('denied');
+                }
+            }
+        }
         // show video
         return view('showVideo')->with(['video' => $video]);
     }
