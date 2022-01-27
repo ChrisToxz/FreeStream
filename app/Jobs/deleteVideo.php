5b6080cb5178c8beac1e7b16aab7b5e686b09d33
Chris Toxz <1christix@gmail.com>
2022-01-27 20:04:46 +0100

    delete video job

---
+++ app/Jobs/deleteVideo.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Video;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class deleteVideo implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public Video $video;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Video $video)
+    {
+        $this->video = $video;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        \Storage::disk('videos')->deleteDirectory($this->video->tag);
+        $this->video->deleteOrFail();
+    }
+}
