797a94ba09746610938fea9e8c8ac46f761df46b
Chris Toxz <1christix@gmail.com>
2022-01-29 18:20:44 +0100

    footer

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,6 +9,15 @@
             <livewire:show-videos/>
 
     </div>
+    <footer class="text-muted py-5">
+        <div class="container">
+            <p class="float-end mb-1">
+                <a href="#">Back to top</a>
+            </p>
+            <p class="mb-1">Powered by <a href="">Slipstream</a></p>
+            <p class="mb-0"></p>
+        </div>
+    </footer>
     <script>
         function copy(text) {
             // TODO: proper copied feedback.
