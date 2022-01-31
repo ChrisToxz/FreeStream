1c3c5d102930df0ee7b43b7edada549b4450bf95
Chris Toxz <1christix@gmail.com>
2022-01-31 18:03:52 +0100

    perf: Kernel updated

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -22,6 +22,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('slipstream:checkretention')->everyTwoMinutes();
     }
 
     /**
