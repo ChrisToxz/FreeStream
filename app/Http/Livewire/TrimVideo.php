740322e665b1e3a7db7c2051fa4c0f013a99c0b4
Chris Toxz <1christix@gmail.com>
2022-02-07 20:05:43 +0100

    dev of video trimming

--- app/Http/Livewire/TrimVideo.php
+++ app/Http/Livewire/TrimVideo.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use App\Models\Video;
+use FFMpeg\Coordinate\TimeCode;
 use Livewire\Component;
 
 class TrimVideo extends Component
@@ -12,6 +13,13 @@ class TrimVideo extends Component
     public function mount(Video $video)
     {
         $this->video = $video;
+    }
+
+    public function trim(){
+        $ff = \FFMpeg::fromDisk('videos')->open($this->video->originalpath);
+        $ff->filters()->clip(TimeCode::fromSeconds(1), TimeCode::fromSeconds(5));
+        $ff->export()->toDisk('videos')->inFormat(new \FFMpeg\Format\Video\X264())->save('trimmed'.qu$this->video->originalpath);
+
     }
 
     public function render()
--- resources/views/livewire/trim-video.blade.php
+++ resources/views/livewire/trim-video.blade.php
@@ -23,7 +23,7 @@
         </div>
         <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-{{--            <button type="button" wire:click.prevent="update({{$video->id}})" data-dismiss="modal" class="btn btn-primary">Save changes</button>--}}
+            <button type="button" wire:click.prevent="trim()"  class="btn btn-primary">Save changes</button>
         </div>
     </div>
 </div>
