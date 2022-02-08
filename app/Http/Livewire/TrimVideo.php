bdfba11081da46db5d12bcf656a9667492910e97
Chris Toxz <1christix@gmail.com>
2022-02-08 19:20:17 +0100

    dev of trimming video

--- app/Http/Livewire/TrimVideo.php
+++ app/Http/Livewire/TrimVideo.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Livewire;
 
+use App\Enums\VideoType;
+use App\Jobs\x264Optimization;
 use App\Models\Video;
 use FFMpeg\Coordinate\TimeCode;
+use Illuminate\Support\Str;
 use Livewire\Component;
 
 class TrimVideo extends Component
@@ -16,10 +19,24 @@ class TrimVideo extends Component
     }
 
     public function trim(){
-        $ff = \FFMpeg::fromDisk('videos')->open($this->video->originalpath);
-        $ff->filters()->clip(TimeCode::fromSeconds(1), TimeCode::fromSeconds(5));
-        $ff->export()->toDisk('videos')->inFormat(new \FFMpeg\Format\Video\X264())->save('trimmed'.$this->video->originalpath);
+        $new_originalhash = Str::random(40);
+        $old_originalpath = $this->video->editableVideoPath;
 
+        $ff = \FFMpeg::fromDisk('videos')->open($old_originalpath);
+        $ff->filters()->clip(TimeCode::fromSeconds(1), TimeCode::fromSeconds(5));
+        $ff->export()->toDisk('videos')->inFormat(new \FFMpeg\Format\Video\X264())->save($this->video->tag.'/'.$new_originalhash.'.mp4');
+
+        $this->video->original = $new_originalhash.'.mp4';
+        $this->video->save();
+        if($this->video->type != VideoType::Original()){
+            $files = \Storage::disk('videos')->files($this->video->tag.'/stream');
+            $matchingFiles = preg_grep('/.*?'.mb_substr($this->video->streamhash, 0, -5).'?/', $files);
+            foreach($matchingFiles as $file){
+                \Storage::disk('videos')->delete($file);
+            }
+            x264Optimization::dispatch($this->video, $this->video->type);
+        }
+        \Storage::disk('videos')->delete($old_originalpath);
     }
 
     public function render()
