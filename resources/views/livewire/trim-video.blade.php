7c6f9bd7a35e51b0b8c3969a1bab567c2b6a8f34
Chris Toxz <1christix@gmail.com>
2022-02-10 18:09:28 +0100

    fix: Unwanted change of values when using trim video range slider

--- resources/views/livewire/trim-video.blade.php
+++ resources/views/livewire/trim-video.blade.php
@@ -16,18 +16,17 @@
                 @error('start') <span class="error">{{ $message }}</span> @enderror
                 @error('end') <span class="error">{{ $message }}</span> @enderror
 
+            <style>
+                /* Supress pointer events */
+                #slider-range { pointer-events: none; }
+                /* Enable pointer events for slider handle only */
+                #slider-range .ui-slider-handle { pointer-events: auto; }
+            </style>
             <form class="row justify-content-md-center mt-3">
                     <div class="col-md-10">
                         <span class="text-black" id="amount"></span>
                         <span class="text-body float-end" id="duration"></span>
                         <div class="mt-1" id="slider-range"></div>
-                    </div>
-
-                    <div class="col-10">
-                            <p class="text-body">
-
-                            </p>
-
                     </div>
             </form>
 
@@ -49,10 +48,10 @@
                     max: {{ $video->info->duration }},
                     values: [ 0, {{ $video->info->duration }} ],
                     slide: function( event, ui ) {
-                        var start = new Date($( "#slider-range" ).slider( "values", 0 ) * 1000).toISOString().substr(11, 8);
-                        var end = new Date($( "#slider-range" ).slider( "values", 1 ) * 1000).toISOString().substr(11, 8);
+                        var start = new Date(ui.values[0] * 1000).toISOString().substr(11, 8);
+                        var end = new Date(ui.values[1] * 1000).toISOString().substr(11, 8);
                         $( "#amount" ).text( "Start " + start + " - End " + end );
-                        var duration = new Date(($( "#slider-range" ).slider( "values", 1 )-$( "#slider-range" ).slider( "values", 0 )) * 1000).toISOString().substr(11,8);
+                        var duration = new Date((ui.values[1]-ui.values[0]) * 1000).toISOString().substr(11,8);
                         $( "#duration" ).text("New duration: " + duration);
                     }
                 });
